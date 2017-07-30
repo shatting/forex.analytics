6437c986d509c77ff364dc808354d6ba9810c51e
Kurtis Copley <kurtcop@gmail.com>
2017-07-30 04:02:18 -0500

    tweaks

--- src/TradingSystem.cpp
+++ src/TradingSystem.cpp
@@ -49,7 +49,7 @@ inline double EvaluateFitness(FitnessFunctionArgs args)
 			}
 			else
 			{
-				points -= (maxLoss) * 7; // also scale loss to duration, and weight losses to be considered a bit more carefully
+				points -= (maxLoss) * 5; // also scale loss to duration, and weight losses to be considered a bit more carefully
 				negative++;
 			}
 		}
